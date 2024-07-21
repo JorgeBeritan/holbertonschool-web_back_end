583abe65b7f24b1afad23461ff9ff693af0b9fdd
Jorge Beritan <kbit@jorge.168.1.14>
2024-07-21 07:25:15 -0300

    first commit

---
+++ 0-constants.js
@@ -0,0 +1,16 @@
+export function taskFirst() {
+	const = "I prefer const when I can.";
+	return task;
+}
+
+export function getLast() {
+	return ' is okay';
+}
+
+export function taskMext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
+
