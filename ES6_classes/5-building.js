c9242442e45866e85c59e114a153148c076840c5
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-26 11:29:19 -0300

    First commit

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -9,11 +9,4 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    this._sqft = value;
-  }
 }
