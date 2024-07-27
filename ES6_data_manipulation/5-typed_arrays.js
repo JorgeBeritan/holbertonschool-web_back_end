9bffe13fdfbfec90b4e535a96ba4c0cb40588704
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-27 10:26:43 -0300

    First commit

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 export default function createInt8TypeArray(length, position, value) {
   const int8 = new Array(length);
-  return int8[position] = value;
+  int8[position] = value;
+  return int8;
 }
