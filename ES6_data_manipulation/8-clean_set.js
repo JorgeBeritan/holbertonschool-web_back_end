05a0e8aecb3e888a86e220fd287f099c24c0a2c6
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-27 11:10:26 -0300

    First commit

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, string) {
+  if (typeof string !== 'string' || string === '') {
+    return '';
+  }
+
+  const result = '';
+  
+  for (let i of set) {
+    if (typeof i === 'string' && i.startWith(string)) {
+      result.push(i.slice(string.lenght));
+	}
+  }
+
+  return result.join('-');
+}
