bc28392a3c0234622fee0174e68f04f7bd0dcaf1
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-27 10:35:32 -0300

    add buffer array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
 export default function createInt8TypeArray(length, position, value) {
-  const int8 = new Int8Array(length);
-  if (position > int8.length) {
+  const buffer = new ArrayBuffer(length); 
+  if (position > buffer.length) {
     throw new Error('Position outside range');
   }
+  const int8 = new Int8Array(buffer);
   int8[position] = value;
   return int8;
 }
