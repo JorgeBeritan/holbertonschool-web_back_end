bd4319b1b2299426584b01818f8d58990ed15b0b
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-27 10:32:12 -0300

    Fix porblem with array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypeArray(length, position, value) {
-  const int8 = new Array(length);
+  const int8 = new Int8Array(length);
   if (position > int8.length) {
-	throw new Error('Position outside range');
+    throw new Error('Position outside range');
   }
   int8[position] = value;
   return int8;
