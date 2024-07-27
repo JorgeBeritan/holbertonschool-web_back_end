322d61d2abad9096a7f4d105add016f4807e373c
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-27 11:25:00 -0300

    another form

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,12 @@ export default function cleanSet(set, string) {
   if (typeof string !== 'string' || string === '') {
     return '';
   }
+  const reault = [];
+  for (const value of set) {
+    if (value.startsWith(string)) {
+      result.push(value.slice(string.length));
+	}
+  }
 
-  const filterData = [...set].filter((value) => typeof value === 'string' && value.startsWith(string));
-  const mapData = filterData.map((value) => value.slice(string.lenght));
-
-  return mapData.join('-');
+  return result.join('-');
 }
