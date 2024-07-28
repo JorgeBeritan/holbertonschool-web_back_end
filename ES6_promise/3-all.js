c5015cccfb08beb3385a8878c3b0e98afc717760
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-28 12:00:18 -0300

    First commit

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,8 @@ export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((response) => {
-      console.log(`${response[0].body} ${response[1].firstName} ${response[2].lastName}`);
+      const [photoResponse, userResponse] = response;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
