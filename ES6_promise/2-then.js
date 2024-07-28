5c85409da9f980e0be64b996479b63020aed4fbd
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-28 11:32:33 -0300

    Fix handle catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(response => {
+    .then(() => {
       console.log('Got a response from API');
-      return ({status: 200, body: 'success'});
-	});
-    .cath(reject => {
+      return ({ status: 200, body: 'success' });
+    })
+    .cath(() => {
       console.log('Got a response from API');
       return new Error();
-	});
+    })
     .finally(() => {
       console.log('Got a response from API');
-	});
+    });
 }
