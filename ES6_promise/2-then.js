7ae4142035624cb297dd98abed613a1e040807bd
JorgeBeritan <inaikmcontacto@gmail.com>
2024-07-28 11:33:34 -0300

    remove console.log in then and cathc

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from API');
-      return ({ status: 200, body: 'success' });
-    })
-    .cath(() => {
-      console.log('Got a response from API');
-      return new Error();
-    })
+    .then(() => ({ status: 200, body: 'success' }))
+    .cath(() => new Error())
     .finally(() => {
       console.log('Got a response from API');
     });
