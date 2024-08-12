d44f2fa1e8fb48da8b01848b30e699031a630866
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 13:53:34 -0300

    First commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Made By me
+"""
+
+
+def concat(x: str, y: str) -> str:
+    """
+    Concat two string
+
+    Parameters:
+    x(str): The first string to concat
+    y(str): The second string to concat
+
+    Return:
+    str: a concate string.
+    """
+    return x + y
