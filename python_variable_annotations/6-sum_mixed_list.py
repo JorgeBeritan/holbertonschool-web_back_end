ce744934b12de879c55fb159ff3e565ea475c508
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 14:20:15 -0300

    First commit

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Made By Me
+"""
+
+
+from typing import List, Union
+
+def sum_mixed_list(list: List[Union[int, float]]) -> float:
+    """
+    Sum a mixed list
+
+    Parameters:
+    list(List[Union[int, float]]): First argument
+
+    Return:
+    float: a sum of a mixed list
+    """
+    return sum(list)
