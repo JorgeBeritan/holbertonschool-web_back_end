8fc8cd0d8d604e8ed53deaf0515cd76dd59e8a74
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 13:38:28 -0300

    Add documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 def add(x: float, y: float) -> float:
+    """In this function we take 2 argument
+        the first argument take a variable x this is a float
+        and the second argument we take a variable y is a float
+        and we sum this varibales
+    """
     return x + y
