af2e7a5ee56ba71eb0b000b119e6ba40a602441d
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 14:00:25 -0300

    First commit

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Made By Me
+"""
+
+
+def to_string(x: float) -> str:
+    """
+    Parameters:
+    x(float): The first arguments
+
+    Returns:
+    str: return a string representation of a float
+    """
+    return str(x)
