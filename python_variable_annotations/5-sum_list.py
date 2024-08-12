a29e452b4cfa77db15856055f20290b3cbf15491
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 14:12:30 -0300

    First commit

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """
     Made By Me
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Sum a list of float
 
