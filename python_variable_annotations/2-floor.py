b3fd47a861bccc550943a3d961b6c0715409994d
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 13:57:33 -0300

    First commit

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Made By me
+"""
+
+
+def floor(x: float) -> int:
+    """
+    Parameters:
+    x(float): the first argument
+
+    Return:
+    int: convert the float into an integer
+    """
+    return int(x)
