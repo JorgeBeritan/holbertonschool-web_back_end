d4fbc7a78632e18fde68dd1e7c0e2ac1960fdc3a
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-12 14:11:16 -0300

    First commit

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Made By Me
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Sum a list of float
+
+    Parameters:
+    input_list(list[float]): first argument
+
+    Return:
+    float: retrun a sum of a list_input
+    """
+    return sum(input_list)
