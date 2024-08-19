d8df3f3f8c12ceca7f6a08901398bcb67a30580d
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-19 09:29:06 -0300

    pycodestyle fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,15 +4,15 @@
 """
 
 
+from typing import AsyncGenerator
 import asyncio
 async_generator = __import__('0-async_generator').async_generator
-from typing import AsyncGenerator
 
 
 async def async_comprehension() -> AsyncGenerator[float, None]:
     """
     Return:
-    float: a list of float-pointer number 
+    float: a list of float-pointer number
     """
     result = [number async for number in async_generator()]
     return result
