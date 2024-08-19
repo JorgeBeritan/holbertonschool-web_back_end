f844acfae0f5122010b293aa03ff99c78a100abf
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-19 08:57:17 -0300

    Readme example

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Made By Me
+"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Yield a random number between 1 and 10,
+    in this function we learn how to use yield in async function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
