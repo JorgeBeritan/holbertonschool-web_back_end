a7e1ee00a223b53d6d666a3538515b9f288822ae
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-24 08:01:41 -0300

    Fix generator

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,11 @@
 """
 
 
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Return:
     float: a list of float-pointer number
