2fbb6efc58938d27283c991f43d5b4b84a1220da
JorgeBeritan <inaikmcontacto@gmail.com>
2024-08-24 07:52:52 -0300

    pycodestyle fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """
     Yield a random number between 1 and 10,
     in this function we learn how to use yield in async function
