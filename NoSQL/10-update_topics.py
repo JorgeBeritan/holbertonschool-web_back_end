da2709e8d04086d73201c421353155722944e357
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-09-04 10:13:55 -0300

    change {}

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -12,9 +12,8 @@ def update_topics(mongo_collection, name, topics):
     Document??
     """
 
-    documents = mongo_collection.update_many({
-        {"name": name},
-        {"$set": {"topics": topics}}
-    })
+    documents = mongo_collection.update_many(
+        {"name": name}, {"$set" : {"topics": topics}}
+        )
 
     return documents.modified_count
