e9e77cd022a33bedd241f9e81a5d058401033e64
JorgeBeritan <inaikmcontacto@gmail.com>
2024-09-22 19:46:43 -0300

    First commit

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,14 @@
 const app = require('http');
 
 app.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+  const url = req.url;
+
+  if (url == '/') {
+    res.write("Hello Holberton School");
+    res.end()
+  } else if (url == '/test') {
+    res.write("Hello Holberton School");
+    res.end();
+  }
 }).listen(1245);
 module.exports = app;
