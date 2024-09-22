1d1e6c5fac40102ffc8260d9c933bfd5b41ecf0f
JorgeBeritan <inaikmcontacto@gmail.com>
2024-09-22 06:54:46 -0300

    First commit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,18 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question(`Welcome to Holberton School, what is your name? `, name => {
-    console.log(`Your name is: ${name}`);
-    console.log(`This important software is now closing`);
-    rl.close();
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
 });
