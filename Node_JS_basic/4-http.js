16a691b83cdc4ba482aa9de45bb01df9db5c08e2
JorgeBeritan <inaikmcontacto@gmail.com>
2024-09-22 20:10:50 -0300

    fix endpoint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,12 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
-  
-  res.writeHead(200, { 'Content-Type' : 'text/html' });
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School')
+});
 
-  const url = req.url;
-
-  if (url == '/') {
-    res.write("Hello Holberton School");
-    res.end()
-  } else if (url == '/test') {
-    res.write("Hello Holberton School");
-    res.end();
-  }
-}).listen(1245);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 module.exports = app;
